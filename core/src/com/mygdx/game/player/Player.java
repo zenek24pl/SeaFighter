9ec29ee2f059da72affa2fdbd9243f71687e7c3b
AndrzejWojtani <awojtania@9bits.com.pl>
2017-03-15 23:38:05 +0100

    Player added

---
+++ core/src/com/mygdx/game/player/Player.java
@@ -0,0 +1,26 @@
+package com.mygdx.game.player;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+/**
+ * Created by zenek on 15.03.2017.
+ */
+
+public class Player extends Image {
+
+    private final static int WIDTH=88;
+    private final static int HEIGHT=150;
+
+    private final static int STARTING_X=200;
+    private final static int STARTING_Y=300;
+
+    public Player(){
+        super(new Texture("badlogic.jpg"));
+
+        this.setOrigin(WIDTH/2,HEIGHT/2);
+        this.setSize(WIDTH,HEIGHT);
+
+        this.setPosition(STARTING_X,STARTING_Y);
+    }
+}
