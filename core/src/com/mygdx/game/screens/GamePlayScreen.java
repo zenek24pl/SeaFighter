073589a627f9f9710f9fa6e519e672c59d763967
AndrzejWojtani <awojtania@9bits.com.pl>
2017-03-15 20:22:56 +0100

    game play init

---
+++ core/src/com/mygdx/game/screens/GamePlayScreen.java
@@ -0,0 +1,17 @@
+package com.mygdx.game.screens;
+
+import com.mygdx.game.MyGdxGame;
+
+/**
+ * Created by zenek on 15.03.2017.
+ */
+
+public class GamePlayScreen extends AbstractScreen {
+    public GamePlayScreen(MyGdxGame game) {
+        super(game);
+        init();
+    }
+    private void init(){
+        
+    }
+}
